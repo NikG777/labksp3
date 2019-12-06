aa249e2b11ec1161cbe7b153375842af9844385a
Nikita <43072566+NikG777@users.noreply.github.com>
2019-12-06 22:45:18 +0300

    spring security

--- src/main/java/com/example/labksp/LabkspApplication.java
+++ src/main/java/com/example/labksp/LabkspApplication.java
@@ -2,7 +2,7 @@ package com.example.labksp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 public class LabkspApplication {
 
---
+++ src/main/java/com/example/labksp/Role.java
@@ -0,0 +1,5 @@
+package com.example.labksp;
+
+public enum Role {
+    USER;
+}
